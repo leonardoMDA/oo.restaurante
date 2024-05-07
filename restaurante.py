f2ea668e0868edaad4601100dfff5faa9743387c
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-05-07 10:05:30 -0300

    Add files via upload

---
+++ restaurante.py
@@ -0,0 +1,17 @@
+class Restaurante:
+ def __init__(self,nome,categoria):
+    self.nome = nome
+    self.categoria = categoria
+    self.ativo = False
+
+def __str__(self):
+    return f'{self.nome} | {self.categoria}'
+
+restaurante_praca = Restaurante('Praça','Gourmet')
+restaurante_pizza = Restaurante('Pizza', 'Italiana')
+
+restaurantes = [restaurante_pizza,restaurante_praca]
+
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
+
