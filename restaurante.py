724b282966e46552e1d2a99b2ba2827a912a75ea
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-05-17 11:27:01 -0300

    Add files via upload

--- restaurante.py
+++ restaurante.py
@@ -1,11 +1,23 @@
 class Restaurante:
- def __init__(self,nome,categoria):
-    self.nome = nome
-    self.categoria = categoria
-    self.ativo = False
+    restaurantes = []
 
-def __str__(self):
-    return f'{self.nome} | {self.categoria}'
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+    def listar_restaurantes():
+     print(f'{"nome do restaurante".ljust(25)} | {"categoria".ljust(25)} | {"status".ljust(25)}')
+     for restaurante in Restaurante.restaurantes:
+        print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
 
 restaurante_praca = Restaurante('Praça','Gourmet')
 restaurante_pizza = Restaurante('Pizza', 'Italiana')
@@ -14,4 +26,3 @@ restaurantes = [restaurante_pizza,restaurante_praca]
 
 print(vars(restaurante_praca))
 print(vars(restaurante_pizza))
-
