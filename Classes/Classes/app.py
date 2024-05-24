ba99d7d9ac7f446ded0c438522b7f9080ff163ee
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-05-24 11:20:54 -0300

    Add files via upload

---
+++ Classes/Classes/app.py
@@ -0,0 +1,15 @@
+from Classes.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_mexicano.alternar_estado()
+restaurante_praca.receber_avaliacao('Gui,10')
+restaurante_praca.receber_avaliacao('José,5')
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
